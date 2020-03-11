45153bc30da0c6324c8ac1b5a6adfac9914da7c0
Chen Zhao <zhaochen17@mails.ucas.ac.cn>
2020-03-12 06:04:04 +0800

    update

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,13 @@
 using YaoQDNN
+using Yao
 using Test
 
-@testset "YaoQDNN.jl" begin
-    # Write your own tests here.
+@testset "layer.jl" begin
+    ql_1 = QNNL{Float64}(hardware_efficient, 8, 8, 20, "XYZ")
+    ql_2 = QNNL{Float64}(hardware_efficient, 6, 6, rand(12), "YZ", rand(12))
+
+    x = rand(8)
+
+    YaoQDNN.forward(ql_1, x)
+    YaoQDNN.back_propagation(ql_1, x)
 end
