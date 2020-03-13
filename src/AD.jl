490895ffe6626e3799214e570e47d3ab3392372b
Chen Zhao <zhaochen17@mails.ucas.ac.cn>
2020-03-14 01:42:45 +0800

    forward and backward AD

--- src/AD.jl
+++ src/AD.jl
@@ -1,6 +1,11 @@
-# need to be developed
+using Flux
+import Flux: params
 
-function forward(ql::QNNL{T}, x::Array{T}) where {N, T}
+using Zygote
+import Zygote: @adjoint
+using YaoBlocks.AD
+
+function apply(ql::QNNL{T}, x) where T
 	cir = chain(ql.encoder, ql.transform)
 	n = nqubits(cir)
 	dispatch!(cir, [x; ql.params])
@@ -8,48 +13,82 @@ function forward(ql::QNNL{T}, x::Array{T}) where {N, T}
 	m = size(ql.Hami, 1)
 	psi = zero_state(n)
 	psi |> cir
-	y = [real(expect((ql.Hami[i]), psi)) for i = 1:m]
+	y = T.([real(expect((ql.Hami[i]), psi)) for i = 1:m])
 	if size(ql.bias, 1) > 0
 		y += ql.bias
 	end
 	return y
 end
+(ql::QNNL{T})(x) where {T} = apply(ql, x)
 
-function back_propagation(ql::QNNL{T}, x::Array{T}) where {N,T}
+params(ql::QNNL{T}) where {T} = params(ql.params, ql.bias)
+
+@adjoint apply(ql::QNNL{T}, x) where {T} = (apply(ql,x), h̄ -> forwardAD(ql, x, h̄))
+
+# need to be developed
+
+function forwardAD(ql::QNNL{T}, x, h̄) where {T}
 	m = size(ql.Hami, 1)
 	s1 = size(x, 1)
 	s2 = size(ql.params, 1)
 	s3 = size(ql.bias, 1)
 
-	if s3 > 0
-		L_b = 1
-	else
-		L_b = 0
-	end
-
-	L_x = zeros(s1, m)
-	L_w = zeros(s2, m)
-
+	L_x = zeros(T, s1, m)
+	L_w = zeros(T, s2, m)
 
 	for i = 1:s1
-		x[i] += π/2
-		y_pos = forward(ql, x)
-		x[i] -= π
-		y_neg = forward(ql, x)
-		x[i] += π/2
+		x[i] += T(π/2)
+		y_pos = ql(x)
+		x[i] -= T(π)
+		y_neg = ql(x)
+		x[i] += T(π/2)
 
 		L_x[i, :] = (y_pos - y_neg) / 2
 	end
 
 	for i = 1:s2
-		ql.params[i] += π/2
-		y_pos = forward(ql, x)
-		ql.params[i] -= π
-		y_neg = forward(ql, x)
-		ql.params[i] += π/2
+		ql.params[i] += T(π/2)
+		y_pos = ql(x)
+		ql.params[i] -= T(π)
+		y_neg = ql(x)
+		ql.params[i] += T(π/2)
 
 		L_w[i, :] = (y_pos - y_neg) / 2
 	end
 
-	return L_x, L_w, L_b
+	if s3 > 0
+		return [L_w*T.(h̄), T.(h̄)], L_x*T.(h̄)
+	else
+		return [L_w*T.(h̄), ql.bias], L_x*T.(h̄)
+	end
+end
+
+function backwardAD(ql::QNNL{T}, x, h̄) where {T}
+	encoder = ql.encoder
+	transform = ql.transform
+	ps = ql.params
+
+	n = nqubits(encoder)
+	psi = zero_state(n)
+
+	m = length(ql.Hami)
+
+	x̄ = T[]
+	w̄ = T[]
+	b̄ = length(ql.bias) > 0 ? T.(h̄) : T[]
+
+	dispatch!(encoder, x)
+	dispatch!(transform, ps)
+
+	psi |> encoder
+	psi	|> transform
+
+	Hs = ql.Hami
+
+	D = ArrayReg(sum([expect'(Hs[i], psi).state for i = 1:m] .* h̄))
+
+	apply_back!((psi, D), transform, w̄)
+	apply_back!((psi, D), encoder, x̄)
+
+	return [w̄, b̄], x̄
 end
