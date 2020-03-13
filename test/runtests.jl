f8a7f2a1e84735317df1a79b1562a3f971e9c478
Chen Zhao <zhaochen17@mails.ucas.ac.cn>
2020-03-14 01:41:39 +0800

    add tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,13 +1,38 @@
 using YaoQDNN
 using Yao
+using YaoAD
+using Flux
 using Test
 
+using Zygote
+
 @testset "layer.jl" begin
-    ql_1 = QNNL{Float64}(hardware_efficient, 8, 8, 20, "XYZ")
-    ql_2 = QNNL{Float64}(hardware_efficient, 6, 6, rand(12), "YZ", rand(12))
+    ql_1 = QNNL{Float64}(hardware_efficient, 8, 12, 20, "YZ")
+    ql_2 = QNNL{Float64}(hardware_efficient, 4, 16, 10, "X")
+    m = Chain(ql_1, ql_2)
+    loss(m, x, y) = Flux.mse(m(x), y)
+    opt = ADAM()
+    x = ones(12)
+    y = ones(4)
 
-    x = rand(8)
+    _, back = Zygote.pullback(Flux.mse, m(x), y)
+    m̄ = back(1)[1]
 
-    YaoQDNN.forward(ql_1, x)
-    YaoQDNN.back_propagation(ql_1, x)
+    gs = gradient((m)->loss(m,x,y), m)
+    @test all(gs[1][1][2] .≈ YaoQDNN.forwardAD(ql_2, ql_1(x), m̄)[1])
+
+    # Float32
+    ql_1 = QNNL{Float32}(hardware_efficient, 8, 12, 20, "YZ")
+    ql_2 = QNNL{Float32}(hardware_efficient, 4, 16, 10, "X")
+    m = Chain(ql_1, ql_2)
+    loss(m, x, y) = Flux.mse(m(x), y)
+    opt = ADAM()
+    x = ones(Float32, 12)
+    y = ones(Float32, 4)
+
+    _, back = Zygote.pullback(Flux.mse, m(x), y)
+    m̄ = back(1)[1]
+
+    gs = gradient((m)->loss(m,x,y), m)
+    @test all(gs[1][1][2] .≈ YaoQDNN.forwardAD(ql_2, ql_1(x), m̄)[1])
 end
