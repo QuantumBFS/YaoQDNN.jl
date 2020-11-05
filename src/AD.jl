8f9e2d436344d91e65e7678986ea409266239b06
Chen Zhao <zhaochen17@mails.ucas.ac.cn>
2020-11-05 16:43:34 +0800

    add faithful layer

--- src/AD.jl
+++ src/AD.jl
@@ -5,103 +5,174 @@ using LinearAlgebra
 using Zygote
 import Zygote: @adjoint, AContext
 using YaoBlocks.AD
+using YaoBlocks.AD: apply_back!
+using YaoBlocks: measure
 
 Flux.@functor QNNL
 
 function apply(ql::QNNL{T}, x) where T
-	cir = chain(ql.encoder, ql.transform)
-	n = nqubits(cir)
-	dispatch!(cir, [x; ql.params])
+    cir = chain(ql.encoder, ql.transform)
+    n = nqubits(cir)
+    dispatch!(cir, [x; ql.params])
 
-	m = size(ql.Hami, 1)
-	psi = zero_state(n)
-	psi |> cir
-	y = T.([real(expect((ql.Hami[i]), psi)) for i = 1:m])
-	if size(ql.bias, 1) > 0
-		y += ql.bias
-	end
-	return y
+    m = size(ql.Hami, 1)
+    psi = zero_state(n)
+    psi |> cir
+
+    y = T.([real(expect((ql.Hami[i]), psi)) for i = 1:m])
+    if size(ql.bias, 1) > 0
+        y += ql.bias
+    end
+    return y
 end
 (ql::QNNL{T})(x) where {T} = apply(ql, x)
 
 params(ql::QNNL{T}) where {T} = params(ql.params, ql.bias)
 
 function Zygote._pullback(cx::AContext, ql::QNNL{T}, x) where {T}
-	out = apply(ql, x)
-	out, function pullback(h̄)
-		∇ql, ∇x = backwardAD(ql, x, h̄)
-		∇W = ∇ql[1]
-		∇b = ∇ql[2]
-		Zygote.accum_param(cx, ql.params, ∇W)
-		Zygote.accum_param(cx, ql.bias, ∇b)
-		return ((W = ∇W, b = ∇b), ∇x)
-	end
+    out = apply(ql, x)
+    out, function pullback(h̄)
+        ∇ql, ∇x = backwardAD(ql, x, h̄)
+        ∇W = ∇ql[1]
+        ∇b = ∇ql[2]
+        Zygote.accum_param(cx, ql.params, ∇W)
+        Zygote.accum_param(cx, ql.bias, ∇b)
+        return ((W = ∇W, b = ∇b), ∇x)
+    end
 end
 
-# TODO need a faithful way
 function forwardAD(ql::QNNL{T}, x, h̄) where {T}
-	m = size(ql.Hami, 1)
-	s1 = size(x, 1)
-	s2 = size(ql.params, 1)
-	s3 = size(ql.bias, 1)
+    m = size(ql.Hami, 1)
+    s1 = size(x, 1)
+    s2 = size(ql.params, 1)
+    s3 = size(ql.bias, 1)
 
-	L_x = zeros(T, s1)
-	L_w = zeros(T, s2)
+    L_x = zeros(T, s1)
+    L_w = zeros(T, s2)
 
-	for i = 1:s1
-		x[i] += T(π/2)
-		y_pos = ql(x)
-		x[i] -= T(π)
-		y_neg = ql(x)
-		x[i] += T(π/2)
+    for i = 1:s1
+        x[i] += T(π/2)
+        y_pos = ql(x)
+        x[i] -= T(π)
+        y_neg = ql(x)
+        x[i] += T(π/2)
 
-		L_x[i] = dot((y_pos - y_neg), h̄) / 2
-	end
+        L_x[i] = dot((y_pos - y_neg), h̄) / 2
+    end
 
-	for i = 1:s2
-		ql.params[i] += T(π/2)
-		y_pos = ql(x)
-		ql.params[i] -= T(π)
-		y_neg = ql(x)
-		ql.params[i] += T(π/2)
+    for i = 1:s2
+        ql.params[i] += T(π/2)
+        y_pos = ql(x)
+        ql.params[i] -= T(π)
+        y_neg = ql(x)
+        ql.params[i] += T(π/2)
 
-		L_w[i] = dot((y_pos - y_neg), h̄) / 2
-	end
+        L_w[i] = dot((y_pos - y_neg), h̄) / 2
+    end
 
-	if s3 > 0
-		return [L_w, T.(h̄)], L_x
-	else
-		return [L_w, ql.bias], L_x
-	end
+    if s3 > 0
+        return [L_w, T.(h̄)], L_x
+    else
+        return [L_w, ql.bias], L_x
+    end
 end
 
 function backwardAD(ql::QNNL{T}, x, h̄) where {T}
-	encoder = ql.encoder
-	transform = ql.transform
-	ps = ql.params
+    encoder = ql.encoder
+    transform = ql.transform
+    ps = ql.params
 
-	n = nqubits(encoder)
-	psi = zero_state(n)
+    n = nqubits(encoder)
+    psi = zero_state(n)
 
-	m = length(ql.Hami)
+    m = length(ql.Hami)
 
-	x̄ = T[]
-	w̄ = T[]
-	b̄ = length(ql.bias) > 0 ? T.(h̄) : T[]
+    x̄ = T[]
+    w̄ = T[]
+    b̄ = length(ql.bias) > 0 ? T.(h̄) : T[]
 
-	dispatch!(encoder, x)
-	dispatch!(transform, ps)
+    dispatch!(encoder, x)
+    dispatch!(transform, ps)
 
-	psi |> encoder
-	psi	|> transform
+    psi |> encoder
+    psi	|> transform
 
-	Hs = ql.Hami
+    Hs = ql.Hami
 
-	D = ArrayReg(sum([expect'(Hs[i], psi).state for i = 1:m] .* h̄))
-	D.state *= 2
+    D = ArrayReg(sum([expect'(Hs[i], psi).state for i = 1:m] .* h̄))
+    D.state *= 2
 
-	apply_back!((psi, D), transform, w̄)
-	apply_back!((psi, D), encoder, x̄)
+    apply_back!((psi, D), transform, w̄)
+    apply_back!((psi, D), encoder, x̄)
 
-	return [w̄, b̄], x̄
+    return [w̄, b̄], x̄
+end
+
+Flux.@functor QNNL_faithful
+
+function apply(ql::QNNL_faithful{T}, x) where T
+    cir = chain(ql.encoder, ql.transform)
+    n = nqubits(cir)
+    dispatch!(cir, [x; ql.params])
+
+    m = size(ql.Hami, 1)
+    psi = zero_state(n)
+    psi |> cir
+
+    y = T.([real(sum(measure(ql.Hami[i], psi; nshots = ql.nshots))) / ql.nshots for i = 1:m])
+    if size(ql.bias, 1) > 0
+        y += ql.bias
+    end
+    return y
+end
+(ql::QNNL_faithful{T})(x) where {T} = apply(ql, x)
+
+params(ql::QNNL_faithful{T}) where {T} = params(ql.params, ql.bias)
+
+function Zygote._pullback(cx::AContext, ql::QNNL_faithful{T}, x) where {T}
+    out = apply(ql, x)
+    out, function pullback(h̄)
+        ∇ql, ∇x = faithfulAD(ql, x, h̄)
+        ∇W = ∇ql[1]
+        ∇b = ∇ql[2]
+        Zygote.accum_param(cx, ql.params, ∇W)
+        Zygote.accum_param(cx, ql.bias, ∇b)
+        return ((W = ∇W, b = ∇b), ∇x)
+    end
+end
+
+function faithfulAD(ql::QNNL_faithful{T}, x, h̄) where {T}
+    m = size(ql.Hami, 1)
+    s1 = size(x, 1)
+    s2 = size(ql.params, 1)
+    s3 = size(ql.bias, 1)
+
+    L_x = zeros(T, s1)
+    L_w = zeros(T, s2)
+
+    for i = 1:s1
+        x[i] += T(π/2)
+        y_pos = ql(x)
+        x[i] -= T(π)
+        y_neg = ql(x)
+        x[i] += T(π/2)
+
+        L_x[i] = dot((y_pos - y_neg), h̄) / 2
+    end
+
+    for i = 1:s2
+        ql.params[i] += T(π/2)
+        y_pos = ql(x)
+        ql.params[i] -= T(π)
+        y_neg = ql(x)
+        ql.params[i] += T(π/2)
+
+        L_w[i] = dot((y_pos - y_neg), h̄) / 2
+    end
+
+    if s3 > 0
+        return [L_w, T.(h̄)], L_x
+    else
+        return [L_w, ql.bias], L_x
+    end
 end
--- src/layer.jl
+++ src/layer.jl
@@ -3,53 +3,67 @@ using Yao
 include("circuit.jl")
 include("hamiltonians.jl")
 
-export QNNL
+export QNNL, QNNL_faithful
 export Layer
 
 abstract type Layer{T<:Real} end
 
 struct QNNL{T} <: Layer{T}
-	encoder::YaoBlocks.ChainBlock
-	transform::YaoBlocks.ChainBlock
-	params::Array{T}
-	Hami::Array{<:YaoBlocks.AbstractBlock, 1}
-	bias::Array{T,1}
+    encoder::YaoBlocks.ChainBlock
+    transform::YaoBlocks.ChainBlock
+    params::Array{T}
+    Hami::Array{<:YaoBlocks.AbstractBlock, 1}
+    bias::Array{T,1}
 end
 
 function QNNL{T}(he::HardwareEfficientAnsatz, nbit::Integer, in_dim::Integer,
-	 			w::Vector{T}, Hami::Array{<:YaoBlocks.AbstractBlock, 1},
-				b::Vector{T}) where {T}
-	cir_e = YaoQDNN.encoder_circuit(he, nbit, in_dim)
-	cir_t = YaoQDNN.transform_circuit(he, nbit, length(w))
-	QNNL{T}(cir_e, cir_t, w, Hami, b)
+                 w::Vector{T}, Hami::Array{<:YaoBlocks.AbstractBlock, 1},
+                b::Vector{T}) where {T}
+    cir_e = YaoQDNN.encoder_circuit(he, nbit, in_dim)
+    cir_t = YaoQDNN.transform_circuit(he, nbit, length(w))
+    QNNL{T}(cir_e, cir_t, w, Hami, b)
 end
 
 function QNNL{T}(he::HardwareEfficientAnsatz, nbit::Integer, in_dim::Integer,
-				npara::Integer, Hami::Array{<:YaoBlocks.AbstractBlock, 1};
-				no_bias = false) where {T}
-	w = T.((rand(npara) .- 0.5) * 2π)
-	if no_bias
-		b = T[]
-	else
-		b = T.(rand(length(Hami)) .- 0.5)
-	end
-	QNNL{T}(he, nbit, in_dim, w, Hami, b)
+                npara::Integer, Hami::Array{<:YaoBlocks.AbstractBlock, 1};
+                no_bias = false) where {T}
+    w = T.((rand(npara) .- 0.5) * 2π)
+    if no_bias
+        b = T[]
+    else
+        b = T.(rand(length(Hami)) .- 0.5)
+    end
+    QNNL{T}(he, nbit, in_dim, w, Hami, b)
 end
 
 function QNNL{T}(he::HardwareEfficientAnsatz, nbit::Integer, in_dim::Integer,
-				w::Vector{T}, Hs::String, b::Vector{T}) where {T}
-	Hami = YaoBlocks.ChainBlock[]
-	for c in Hs
-		Hami = [Hami; genH(nbit, c)]
-	end
-	QNNL{T}(he, nbit, in_dim, w, Hami, b)
+                w::Vector{T}, Hs::String, b::Vector{T}) where {T}
+    Hami = YaoBlocks.ChainBlock[]
+    for c in Hs
+        Hami = [Hami; genH(nbit, c)]
+    end
+    QNNL{T}(he, nbit, in_dim, w, Hami, b)
 end
 
 function QNNL{T}(he::HardwareEfficientAnsatz, nbit::Integer, in_dim::Integer,
-				npara::Integer, Hs::String; no_bias = false) where {T}
-	Hami = YaoBlocks.ChainBlock[]
-	for c in Hs
-		Hami = [Hami; genH(nbit, c)]
-	end
-	QNNL{T}(he, nbit, in_dim, npara, Hami; no_bias = no_bias)
+                npara::Integer, Hs::String; no_bias = false) where {T}
+    Hami = YaoBlocks.ChainBlock[]
+    for c in Hs
+        Hami = [Hami; genH(nbit, c)]
+    end
+    QNNL{T}(he, nbit, in_dim, npara, Hami; no_bias = no_bias)
+end
+
+struct QNNL_faithful{T} <: Layer{T}
+    encoder::YaoBlocks.ChainBlock
+    transform::YaoBlocks.ChainBlock
+    params::Array{T}
+    Hami::Array{<:YaoBlocks.AbstractBlock, 1}
+    bias::Array{T,1}
+    nshots::Int
+end
+
+function QNNL_faithful(ql::QNNL{T}, nshots = 1000) where {T}
+    return QNNL_faithful{T}(copy(ql.encoder), copy(ql.transform),
+        copy(ql.params), deepcopy(ql.Hami), copy(ql.bias), nshots)
 end
